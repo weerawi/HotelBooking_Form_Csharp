bc06731085bb486e453cdaac825262282b844eb9
weerawi <weerawikramatharindu@gmail.com>
2023-06-05 10:30:08 +0530

    application is done

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Q2_CS19020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxRmType = new System.Windows.Forms.ComboBox();
+            this.textBoxDays = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxBook = new System.Windows.Forms.ComboBox();
+            this.textBoxTot = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxRooms = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxRmType
+            // 
+            this.comboBoxRmType.FormattingEnabled = true;
+            this.comboBoxRmType.Location = new System.Drawing.Point(275, 292);
+            this.comboBoxRmType.Name = "comboBoxRmType";
+            this.comboBoxRmType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxRmType.TabIndex = 27;
+            // 
+            // textBoxDays
+            // 
+            this.textBoxDays.Location = new System.Drawing.Point(275, 393);
+            this.textBoxDays.Name = "textBoxDays";
+            this.textBoxDays.Size = new System.Drawing.Size(121, 22);
+            this.textBoxDays.TabIndex = 26;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(275, 209);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(396, 22);
+            this.textBoxAddress.TabIndex = 25;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(275, 127);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(396, 22);
+            this.textBoxName.TabIndex = 24;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(756, 366);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(2);
+            this.btnExit.Size = new System.Drawing.Size(100, 35);
+            this.btnExit.TabIndex = 23;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(756, 246);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Padding = new System.Windows.Forms.Padding(2);
+            this.btnClear.Size = new System.Drawing.Size(100, 35);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCal
+            // 
+            this.btnCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.Location = new System.Drawing.Point(756, 127);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Padding = new System.Windows.Forms.Padding(2);
+            this.btnCal.Size = new System.Drawing.Size(225, 35);
+            this.btnCal.TabIndex = 21;
+            this.btnCal.Text = "Claculate the Price";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 395);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Number of Days";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(96, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Room Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(96, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(413, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Booking Form";
+            // 
+            // comboBoxBook
+            // 
+            this.comboBoxBook.FormattingEnabled = true;
+            this.comboBoxBook.Location = new System.Drawing.Point(496, 477);
+            this.comboBoxBook.Name = "comboBoxBook";
+            this.comboBoxBook.Size = new System.Drawing.Size(80, 24);
+            this.comboBoxBook.TabIndex = 31;
+            // 
+            // textBoxTot
+            // 
+            this.textBoxTot.Location = new System.Drawing.Point(756, 481);
+            this.textBoxTot.Name = "textBoxTot";
+            this.textBoxTot.Size = new System.Drawing.Size(130, 22);
+            this.textBoxTot.TabIndex = 30;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(623, 481);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 20);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Total Cost";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(96, 477);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(342, 20);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Do you want to do an another booking ?";
+            // 
+            // textBoxRooms
+            // 
+            this.textBoxRooms.Location = new System.Drawing.Point(275, 346);
+            this.textBoxRooms.Name = "textBoxRooms";
+            this.textBoxRooms.Size = new System.Drawing.Size(121, 22);
+            this.textBoxRooms.TabIndex = 33;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(96, 348);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(160, 20);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Number of Rooms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1013, 539);
+            this.Controls.Add(this.textBoxRooms);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBoxBook);
+            this.Controls.Add(this.textBoxTot);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBoxRmType);
+            this.Controls.Add(this.textBoxDays);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxRmType;
+        private System.Windows.Forms.TextBox textBoxDays;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxBook;
+        private System.Windows.Forms.TextBox textBoxTot;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxRooms;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q2_CS19020
+{
+    public partial class Form1 : Form
+    {
+
+
+        //number of rooms variable
+        private int singleRooms = 10;
+        private int doubleRooms = 7;
+        private int familyRooms = 4;
+        private int suiteRooms = 2; 
+        private decimal total = 0;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxRmType.Items.Add("Single");
+            comboBoxRmType.Items.Add("Double");
+            comboBoxRmType.Items.Add("Suite");
+            comboBoxRmType.Items.Add("Family");
+            Controls.Add(comboBoxRmType);
+
+            comboBoxBook.Items.Add("Yes");
+            comboBoxBook.Items.Add("No");
+            Controls.Add(comboBoxBook);
+
+
+
+            comboBoxRmType.Text = "--Select--";
+            comboBoxBook.Text = "--Y/N--";
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+ 
+
+            string name = textBoxName.Text;
+            string address = textBoxAddress.Text;
+            string booking = this.comboBoxBook.GetItemText(this.comboBoxBook.SelectedItem);
+
+
+             
+             
+                int rooms;
+                if (!int.TryParse(textBoxRooms.Text, out rooms))
+                {
+                    MessageBox.Show("Please enter a valid number of rooms.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int days;
+                if (!int.TryParse(textBoxDays.Text, out days))
+                {
+                    MessageBox.Show("Please enter a valid number of days.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                string Rtype = this.comboBoxRmType.GetItemText(this.comboBoxRmType.SelectedItem);
+
+
+
+                decimal price = 0;
+
+
+                switch (Rtype)
+                {
+
+                    case "Single":
+                        price = 5000.00m;
+
+                        if ((singleRooms) == 0)
+                        {
+                            MessageBox.Show(Rtype + " rooms are not available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        else if ((singleRooms - rooms) < 0)
+                        {
+                            MessageBox.Show("Only " + (singleRooms) + " " + Rtype + " rooms are available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+                        singleRooms -= rooms;
+                        break;
+
+                    case "Double":
+                        price = 7500.00m;
+                        if ((doubleRooms) == 0)
+                        {
+                            MessageBox.Show(Rtype + " rooms are not available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        else if ((doubleRooms - rooms) < 0)
+                        {
+                            MessageBox.Show("Only " + (doubleRooms) + " " + Rtype + " rooms are available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+                        doubleRooms -= rooms;
+                        break;
+
+                    case "Family":
+                        price = 8000.00m;
+                        if ((familyRooms) == 0)
+                        {
+                            MessageBox.Show(Rtype + " rooms are not available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        else if ((familyRooms - rooms) < 0)
+                        {
+                            MessageBox.Show("Only " + (familyRooms) + " " + Rtype + " rooms are available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+                        familyRooms -= rooms;
+                        break;
+
+                    case "Suite":
+                        price = 12500.00m;
+                        if ((suiteRooms) == 0)
+                        {
+                            MessageBox.Show(Rtype + " rooms are not available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        else if ((suiteRooms - rooms) < 0)
+                        {
+                            MessageBox.Show("Only " + (suiteRooms) + " " + Rtype + " rooms are available at the moment", "Out of Rooms", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+                        suiteRooms -= rooms;
+                        break;
+                }
+
+                decimal disRatePrice = 1;
+
+
+                // Calculate the discount, if any
+                if (days > 5 && (Rtype == "Suite" || Rtype == "Double"))
+                {
+
+                    if (Rtype == "Suite")
+                    {
+                        disRatePrice = 0.88m;
+                    }
+                    if (Rtype == "Double")
+                    {
+                        disRatePrice = 0.95m;
+                    }
+
+
+                }
+
+            if (booking == "Yes")
+            {
+                total += (decimal)rooms * (decimal)days * price * disRatePrice;
+
+
+                // Clear the booking inputs
+                textBoxDays.Text = "";
+                textBoxRooms.Text = "";
+                comboBoxRmType.Text = "--Select--";
+                comboBoxBook.Text = "--Y/N--";
+                textBoxTot.Text = total.ToString();
+            }else
+            {
+                total += (decimal)rooms * (decimal)days * price * disRatePrice;
+                textBoxTot.Text = total.ToString();
+                 
+                MessageBox.Show("Total is " + (total) + "! \n" +  "Booking is Succesfull.", "Receipt", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+
+                
+             
+
+             
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxName.Text = "";
+            textBoxAddress.Text = "";
+            textBoxDays.Text = "";
+            textBoxRooms.Text = "";
+            comboBoxRmType.Text = "--Select--";
+            comboBoxBook.Text = "--Y/N--";
+            textBoxTot.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+         
+    }
+}
